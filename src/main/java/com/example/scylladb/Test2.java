744b6462a458f97028c5cd5135ee6664de39e4bb
hhokyung <hk.lee@tg360tech.com>
2022-10-05 00:37:10 +0900

    eras

--- src/main/java/com/example/scylladb/Test2.java
+++ src/main/java/com/example/scylladb/Test2.java
@@ -30,25 +30,14 @@ public class Test2 {
         System.out.println(row.getString("advertisingId"));
 
         String startTime = ScStringUtils.getCurrentTimeOfLog();
-//        for (int ii = 114000 ; ii < 144000; ii++) {
+//        for (int ii = 1 ; ii < 111; ii++) {
 //            String id = "goodId---"+ii;
-//            session.execute("insert into test3.kb_dev_audience_ads_group (adsgroupno, id) values (90, 'goodid---"+ii+"')");
+//            session.execute("insert into test3.kb_dev_audience_ads_group (adsgroupno, id) values (90, 'goodid---"+ii+"'), (91, 'good888--"+ii+"')");
 //        }
-        BatchStatement batch =
-                BatchStatement.newInstance(
-                        DefaultBatchType.LOGGED
-                );
-        System.out.println("start batch");
-        for (int ii = 0; ii < 100; ii++) {
-            SimpleStatement simple = SimpleStatement.newInstance("INSERT INTO test3.kb_dev_audience_ads_group (adsgroupno, id) VALUES (400, 'val33331" + ii + "')");
-//        SimpleStatement simple2 = SimpleStatement.newInstance("INSERT INTO test3.kb_dev_audience_ads_group (adsgroupno, id) VALUES (400, 'va3333l1')");
-            //RegularStatement built = insertInto(TABLE1).value("k", "batchTest2").value("t", "val2");
-            batch.add(simple);
-            System.out.println("good ing..."+ii);
-        }
 
-        session.execute(batch);
-
+//        PreparedStatement preparedStatement = session.prepare("insert into test3.kb_dev_audience_ads_group (adsgroupno, id) values (?, ?)");
+//        BoundStatement boundStatement = new BoundStatementBuilder(preparedStatement).build();
+//        BatchStatement batchableStatement = new BatchStatementBuilder
         String endTime = ScStringUtils.getCurrentTimeOfLog();
 
         System.out.println("## startTime: "+startTime);
